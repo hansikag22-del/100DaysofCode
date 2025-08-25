b042ab19164fd696e56e9456f924a429882ac7ff
Hansika Gupta <hansikag22@gmail.com>
2025-08-25 20:31:11 +0530

    main.c

--- 100DaysofCode(Day-5)/main.c
+++ 100DaysofCode(Day-5)/main.c
@@ -12,7 +12,7 @@ Simple Interest=1050, Compound Interest=1125.76
 
 */
 
-#include <stdio.h>
+/*#include <stdio.h>
 #include <math.h>
 int main()
 {
@@ -27,11 +27,44 @@ int main()
     scanf("%f",&time);
     
     SimpleInterest = (principal * rate * time)/100;
-    CompoundInterest = principal * pow(()
+    CompoundInterest = principal * pow((1 + rate /100),time)-principal;
+    printf("Simple Interest=%.2f, Compund Interest=%.2f\n",SimpleInterest,CompoundInterest);
+    return 0;
+}*/
+
+/*Q10: Write a program to input time in seconds and convert it to hours:minutes:seconds format.
+Sample Test Cases:
+Input 1:
+3661
+Output 1:
+1:1:1
+
+Input 2:
+7322
+Output 2:
+2:2:2
+*/
+
+#include <stdio.h>
+int main()
+{
+    int seconds,hour,minute,sec;
+    printf("Enter time in seconds: ");
+    scanf("%d",&seconds);
     
+    hour = seconds / 3600;
+    seconds = seconds % 3600;
+    minute = seconds / 60;
+    sec = seconds % 60;
     
-    
-    
+    printf("%d:%d:%d\n", hour, minute, sec);
+    return 0;
 }
+
+
+
+
+
+
 
 
