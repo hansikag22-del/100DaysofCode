f4bad23c7bba7e12e997aa6af7a60e44b5951e1b
Hansika Gupta <hansikag22@gmail.com>
2025-11-14 11:28:19 +0530

    Q82.C
    Implement a program to print each character of a string on a new line.

---
+++ 100DaysofCode(Day-41)/Q82.c
@@ -0,0 +1,31 @@
+//Q82: Print each character of a string on a new line.
+
+/*
+Sample Test Cases:
+Input 1:
+Hi
+Output 1:
+H
+i
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+
+    // Input string safely
+    fgets(str, sizeof(str), stdin);
+
+    // Print each character on a new line
+    for (int i = 0; str[i] != '\0'; i++) {
+        // Stop printing if newline is encountered (fgets stores it)
+        if (str[i] == '\n')
+            break;
+
+        printf("%c\n", str[i]);
+    }
+
+    return 0;
+}
