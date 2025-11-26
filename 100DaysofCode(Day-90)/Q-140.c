91911d62feb7cb99e02fd11581159c0a120f38f7
Hansika Gupta <hansikag22@gmail.com>
2025-11-26 19:45:32 +0530

    Q-140.c

---
+++ 100DaysofCode(Day-90)/Q-140.c
@@ -0,0 +1,57 @@
+//Q140: Define a struct with enum Gender and print person's gender.
+
+/*
+Sample Test Cases:
+Input 1:
+Gender=MALE
+Output 1:
+Male
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+// Define enum for gender
+enum Gender { MALE, FEMALE, OTHER };
+
+// Define struct for a person
+struct Person {
+    char name[50];
+    enum Gender gender;
+};
+
+int main() {
+    struct Person p;
+    char input[10];
+
+    // Taking input for gender
+    printf("Enter gender (MALE/FEMALE/OTHER): ");
+    scanf("%s", input);
+
+    if (strcmp(input, "MALE") == 0)
+        p.gender = MALE;
+    else if (strcmp(input, "FEMALE") == 0)
+        p.gender = FEMALE;
+    else if (strcmp(input, "OTHER") == 0)
+        p.gender = OTHER;
+    else {
+        printf("Invalid gender!\n");
+        return 0;
+    }
+
+    // Print gender
+    switch (p.gender) {
+        case MALE:
+            printf("Male\n");
+            break;
+        case FEMALE:
+            printf("Female\n");
+            break;
+        case OTHER:
+            printf("Other\n");
+            break;
+    }
+
+    return 0;
+}
