c829df3659589d085216e64b338eeca5d5d4bceb
Hansika Gupta <hansikag22@gmail.com>
2025-11-26 19:49:37 +0530

    Q-143.c

---
+++ 100DaysofCode(Day-93)/Q-143.c
@@ -0,0 +1,50 @@
+//Q143: Find and print the student with the highest marks.
+
+/*
+Sample Test Cases:
+Input 1:
+3 students: Ravi 101 95, Sita 102 85, Aman 103 88
+Output 1:
+Topper: Ravi (Marks: 95)
+
+*/
+
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll_no;
+    int marks;
+};
+
+int main() {
+    int n, i, topperIndex = 0;
+
+    printf("Enter number of students: ");
+    scanf("%d", &n);
+
+    struct Student students[n];
+
+    // Input student details
+    for (i = 0; i < n; i++) {
+        printf("\nEnter details for Student %d\n", i + 1);
+        printf("Name: ");
+        scanf("%s", students[i].name);
+        printf("Roll: ");
+        scanf("%d", &students[i].roll_no);
+        printf("Marks: ");
+        scanf("%d", &students[i].marks);
+    }
+
+    // Find topper
+    for (i = 1; i < n; i++) {
+        if (students[i].marks > students[topperIndex].marks) {
+            topperIndex = i;
+        }
+    }
+
+    // Print topper
+    printf("\nTopper: %s (Marks: %d)\n", students[topperIndex].name, students[topperIndex].marks);
+
+    return 0;
+}
