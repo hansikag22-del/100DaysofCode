ec9c6c7a021f9235039b2318ae4d126c4d6a8fdd
Hansika Gupta <hansikag22@gmail.com>
2025-11-28 00:18:29 +0530

    Q-150.c

--- 100DaysofCode(Day-100)/Q-150.c
+++ 100DaysofCode(Day-100)/Q-150.c
@@ -1,1 +1,39 @@
+//Q150: Use pointer to struct to modify and display data using -> operator.
 
+/*
+Sample Test Cases:
+Input 1:
+Student pointer modifying values: John 106 91
+Output 1:
+Modified Data: Name: John | Roll: 106 | Marks: 91
+
+*/
+
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll;
+    int marks;
+};
+
+int main() {
+    struct Student s;
+    struct Student *ptr = &s;
+
+    // Taking input using pointer
+    printf("Enter name: ");
+    scanf("%s", ptr->name);
+
+    printf("Enter roll number: ");
+    scanf("%d", &ptr->roll);
+
+    printf("Enter marks: ");
+    scanf("%d", &ptr->marks);
+
+    // Display using pointer
+    printf("Modified Data: Name: %s | Roll: %d | Marks: %d\n",
+           ptr->name, ptr->roll, ptr->marks);
+
+    return 0;
+}
